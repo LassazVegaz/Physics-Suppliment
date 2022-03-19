140e782a219411567489f49dab2e32056011c1f8
Lasindu Nuwanga <lasindunuwanga@gmail.com>
2022-03-19 13:14:38 +0530

    comments added to velocity service

--- VelocityService/src/ps/velocityservice/IVelocityService.java
+++ VelocityService/src/ps/velocityservice/IVelocityService.java
@@ -2,9 +2,40 @@ package ps.velocityservice;
 
 public interface IVelocityService {
 	
-	double calculateFinalVelocity(double initialVelocity, double acceleration, double time);
-	double calculateFinalVelocityWihtoutTime(double initialVelocity, double acceleration, double displacement);
-	double calculateDisplacement(double initialVelocity, double acceleration, double time);
-	double calculateDisplacementWihtoutAcceleration(double initialVelocity, double finalVelocity, double time);
+	/**
+	 * Calculate final velocity
+	 * @param u Initial velocity (ms-1)
+	 * @param a Acceleration (ms-2)
+	 * @param t Time (s)
+	 * @return Velocity (ms-1)
+	 */
+	double calculateFinalVelocity(double u, double a, double t);
+	
+	/**
+	 * Calculate final velocity
+	 * @param u Initial velocity (ms-1)
+	 * @param a Acceleration (ms-2)
+	 * @param s Displacement (m)
+	 * @return Final velocity (ms-1)
+	 */
+	double calculateFinalVelocityWihtoutTime(double u, double a, double s);
+	
+	/**
+	 * Calculate displacement
+	 * @param u Initial velocity (ms-1)
+	 * @param a Acceleration (ms-2)
+	 * @param t Time (s)
+	 * @return Displacement (m)
+	 */
+	double calculateDisplacement(double u, double a, double t);
+	
+	/**
+	 * Calculate displacement
+	 * @param u Initial velocity (ms-1)
+	 * @param v Final velocity (ms-1)
+	 * @param t Time (s)
+	 * @return Displacement (m)
+	 */
+	double calculateDisplacementWihtoutAcceleration(double u, double v, double t);
 
 }
--- VelocityService/src/ps/velocityservice/VelocityService.java
+++ VelocityService/src/ps/velocityservice/VelocityService.java
@@ -3,23 +3,23 @@ package ps.velocityservice;
 public class VelocityService implements IVelocityService {
 
 	@Override
-	public double calculateFinalVelocity(double initialVelocity, double acceleration, double time) {
-		return initialVelocity + (acceleration * time);
+	public double calculateFinalVelocity(double u, double a, double t) {
+		return u + (a * t);
 	}
 
 	@Override
-	public double calculateFinalVelocityWihtoutTime(double initialVelocity, double acceleration, double displacement) {
-		return Math.sqrt(Math.pow(initialVelocity, 2) + (2 * acceleration * displacement));
+	public double calculateFinalVelocityWihtoutTime(double u, double a, double s) {
+		return Math.sqrt(Math.pow(u, 2) + (2 * a * s));
 	}
 
 	@Override
-	public double calculateDisplacement(double initialVelocity, double acceleration, double time) {
-		return (initialVelocity * time) + (0.5 * acceleration * Math.pow(time, 2));
+	public double calculateDisplacement(double u, double a, double t) {
+		return (u * t) + (0.5 * a * Math.pow(t, 2));
 	}
 
 	@Override
-	public double calculateDisplacementWihtoutAcceleration(double initialVelocity, double finalVelocity, double time) {
-		return 0.5 * time * (initialVelocity + finalVelocity);
+	public double calculateDisplacementWihtoutAcceleration(double u, double v, double t) {
+		return 0.5 * t * (u + v);
 	}
 
 }
